3a068a636b1cd5d419a5e1beae015506dee094b0
mrhellbooy <mrhellbooy@gmail.com>
2026-06-16 09:51:29 +0600

    How to Access the Admin Panel Now
    With HashRouter, the routing information is stored in the URL fragment (after the #), which GitHub Pages ignores, allowing the React client-side application to correctly interpret the path.

--- web_admin_panel/frontend/src/AppRouter.jsx
+++ web_admin_panel/frontend/src/AppRouter.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import AdminPanel from './pages/AdminPanel';
 import DownloadPage from './pages/DownloadPage';
 
 export default function AppRouter() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<DownloadPage />} />
           <Route path="admin" element={<AdminPanel />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
